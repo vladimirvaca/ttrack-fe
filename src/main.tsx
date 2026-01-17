395f151ce6747b2d59072a7b5363812ee9931f8c
Vladimir Vaca <ramvlay@gmail.com>
2026-01-17 13:17:54 -0500

    Integrate PrimeReact and PrimeIcons with initial setup and theme styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+
+// PrimeReact styles
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 import Routes from './routes/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Routes />
+    <PrimeReactProvider>
+      <Routes />
+    </PrimeReactProvider>
   </StrictMode>
 );
