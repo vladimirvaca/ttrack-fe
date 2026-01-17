ae411e95c565921d9df9cb64713328ef954e0ad6
Vladimir Vaca <ramvlay@gmail.com>
2026-01-17 13:48:50 -0500

    Add TailwindCSS with initial configuration and PostCSS setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import { PrimeReactProvider } from 'primereact/api';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { PrimeReactProvider } from 'primereact/api';
 
 // PrimeReact styles
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+
+// Global styles
+import './index.css';
 
 import Routes from './routes/Routes.tsx';
 
